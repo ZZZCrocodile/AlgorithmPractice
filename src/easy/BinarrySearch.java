d2de1dc78be0f36713c53479feec26fda99d3d46
lynszhang <lynszhang@wesure.cn>
2021-06-16 15:53:57 +0800

    二叉树递归实现

---
+++ src/easy/BinarrySearch.java
@@ -0,0 +1,30 @@
+package easy;
+
+public class BinarrySearch {
+    public int find(int[] arr,int low,int high,int key){
+       if (low>high || arr[low]<key || arr[high]>key){
+           return -1;
+       }
+       int middle = (low+high)/2;
+       if (arr[middle]>key){
+           find(arr,low,middle-1,key);
+       }else if(arr[middle]<key){
+           find(arr,middle+1,high,key);
+       }else {
+           return middle;
+       }
+       return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,3,5,7,9,11};
+        int key = 4;
+        BinarrySearch binarrySearch = new BinarrySearch();
+        int position = binarrySearch.find(arr,0,arr.length,4);
+        if (position==-1){
+            System.out.println("没找到");
+        }else {
+            System.out.println(position);
+        }
+    }
+}
