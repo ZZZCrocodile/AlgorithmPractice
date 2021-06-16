b59968237c317e7e22157374e920d91a10fd94b8
lynszhang <lynszhang@wesure.cn>
2021-06-16 16:47:52 +0800

    二叉树递归实现-修复传入high没有-1，以及递归中未加上return

--- src/easy/BinarrySearch.java
+++ src/easy/BinarrySearch.java
@@ -2,24 +2,23 @@ package easy;
 
 public class BinarrySearch {
     public int find(int[] arr,int low,int high,int key){
-       if (low>high || arr[low]<key || arr[high]>key){
+       if (low>high || arr[low]>key || arr[high]<key){
            return -1;
        }
        int middle = (low+high)/2;
        if (arr[middle]>key){
-           find(arr,low,middle-1,key);
+           return find(arr,low,middle-1,key);
        }else if(arr[middle]<key){
-           find(arr,middle+1,high,key);
+           return find(arr,middle+1,high,key);
        }else {
            return middle;
        }
-       return -1;
     }
     public static void main(String[] args) {
         int[] arr = {1,3,5,7,9,11};
-        int key = 4;
+        int key = 11;
         BinarrySearch binarrySearch = new BinarrySearch();
-        int position = binarrySearch.find(arr,0,arr.length,4);
+        int position = binarrySearch.find(arr,0,arr.length-1,key);
         if (position==-1){
             System.out.println("没找到");
         }else {
